54db75ad1858c4a485cf3b639b1dbf2f4bb29cf6
Ulathi Ncube <swiftly.graphics@gmail.com>
2023-02-04 19:45:12 +0200

    initial commit

---
+++ src/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.{html,js}'],
+  theme: {
+    fontFamily: {
+      serif: ['Playfair Display', 'sans'],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
